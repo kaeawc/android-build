1b580436a613d09063e9941b4c61e15eea8e7dd8
Jason Pearson <jason.d.pearson@gmail.com>
2023-11-12 04:09:30 -0600

    use version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,13 @@ plugins {
 
 android {
     namespace = "dev.jasonpearson.android"
-    compileSdk = 34
+    compileSdk = libs.versions.build.android.compileSdkVersion.get().toInt()
+    buildToolsVersion = libs.versions.build.android.buildToolsVersion.get()
 
     defaultConfig {
         applicationId = "dev.jasonpearson.android"
-        minSdk = 27
-        targetSdk = 34
+        minSdk = libs.versions.build.android.minSdkVersion.get().toInt()
+        targetSdk = libs.versions.build.android.targetSdkVersion.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
@@ -30,17 +31,18 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.toVersion(libs.versions.build.java.compileVersion.get())
+        targetCompatibility = JavaVersion.toVersion(libs.versions.build.java.compileVersion.get())
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = libs.versions.build.java.compileVersion.get()
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = libs.versions.build.compose.kotlinCompilerVersion.get()
+        useLiveLiterals = false
     }
     packaging {
         resources {
@@ -51,19 +53,15 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.lifecycle.runtime)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.bundles.compose.ui)
+
+    testImplementation(libs.bundles.unit.test)
+
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.bundles.compose.ui.espresso.test)
+
+    debugImplementation(libs.bundles.compose.ui.debug)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,15 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.3.0-alpha13" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    `version-catalog`
+}
+
+buildscript {
+    repositories {
+        google()
+        gradlePluginPortal()
+    }
+
+    dependencies {
+        classpath(libs.agp)
+        classpath(libs.kgp)
+    }
 }
