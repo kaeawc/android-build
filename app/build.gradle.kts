67352108e1523caf6732c77c61ef00aa25a8e943
Jason Pearson <jason.d.pearson@gmail.com>
2024-10-23 04:57:41 -0500

    use SARIF reports for inline PR comments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -107,6 +107,10 @@ android {
     }
     buildFeatures { compose = true }
     packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
+
+    lint {
+        sarifOutput = file("$buildDir/reports/lint-results.sarif")
+    }
 }
 
 tasks.withType<KotlinCompile>().configureEach {
